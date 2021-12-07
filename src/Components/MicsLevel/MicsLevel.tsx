b738adf852012fe63eadf50410e96ef0795c017a
lapprenti18 <pokspid@gmail.com>
2021-12-07 15:21:21 +0100

    feat(Mics level): create component slider bar

--- src/Components/MicsLevel/MicsLevel.tsx
+++ src/Components/MicsLevel/MicsLevel.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactRanger from 'react-ranger'
+import CustomizedSlider from '../Slider/Slider'
 
 export const MicsLevel = () => {
   return (
       <>
+        <CustomizedSlider />
       </>
   )
   // const [values, setValues] = React.useState([10])
--- src/Components/Slider/Slider.tsx
+++ src/Components/Slider/Slider.tsx
@@ -1,46 +1,104 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Slider from "@mui/material/Slider";
+import * as React from 'react';
+import Slider, { SliderThumb } from '@mui/material/Slider';
+import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+interface Props {
+  children: React.ReactElement;
+  value: number;
+}
+
+const iOSBoxShadow =
+  '0 5px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)';
 
 const marks = [
   {
     value: 0,
-    label: "0%"
+    label: "0",
   },
   {
     value: 20,
-    label: "20%"
+    label: "20"
   },
   {
     value: 40,
-    label: "40%"
+    label: "40"
   },
   {
     value: 60,
-    label: "60%"
+    label: "60"
   },
   {
     value: 80,
-    label: "80%"
+    label: "80"
   },
   {
     value: 100,
-    label: "100%"
-  }
+    label: "100"
+  },
 ];
 
-function valuetext(value: number) {
-  return `${value}°C`;
-}
+const IOSSlider = styled(Slider)(({ theme }) => ({
+  color: theme.palette.mode === 'dark' ? '#3880ff' : '#3880ff',
+  height: 2,
+  padding: '15px 0',
+  '& .MuiSlider-thumb': {
+    height: 20,
+    width: 20,
+    backgroundColor: '#eeeeee',
+    boxShadow: iOSBoxShadow,
+    '&:focus, &:hover, &.Mui-active': {
+      boxShadow:
+        '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.3),0 0 0 1px rgba(0,0,0,0.02)',
+      '@media (hover: none)': {
+        boxShadow: iOSBoxShadow,
+      },
+    },
+  },
+  '& .MuiSlider-valueLabel': {
+    fontSize: 12,
+    fontWeight: 'normal',
+    top: -6,
+    backgroundColor: 'unset',
+    color: theme.palette.text.primary,
+    '&:before': {
+      display: 'none',
+    },
+    '& *': {
+      background: 'transparent',
+      color: theme.palette.mode === 'dark' ? '#eeeeee' : '#eeeeee',
+    },
+  },
+  '& .MuiSlider-track': {
+    border: 'none',
+  },
+  '& .MuiSlider-rail': {
+    opacity: 0.5,
+    backgroundColor: '#bfbfbf',
+  },
+  '& .MuiSlider-mark': {
+    backgroundColor: '#bfbfbf',
+    height: 8,
+    width: 1,
+    '&.MuiSlider-markActive': {
+      opacity: 1,
+      backgroundColor: 'currentColor',
+    },
+  },
+  '& .MuiSlider-markLabel': {
+    color: '#eeeeee'
+  }
+}));
 
-export default function DiscreteSliderLabel() {
+
+export default function CustomizedSlider() {
   return (
-    <Box sx={{ width: 300 }}>
-      <Slider
-        aria-label="Always visible"
-        defaultValue={80}
-        getAriaValueText={valuetext}
-        step={1}
+    <Box sx={{ width: 720 }}>
+      <Typography gutterBottom>Micro 1</Typography>
+      <IOSSlider
+        aria-label="Micro"
+        defaultValue={50}
         marks={marks}
         valueLabelDisplay="on"
       />
