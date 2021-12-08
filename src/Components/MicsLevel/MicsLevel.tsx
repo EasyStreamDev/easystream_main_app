694c9e915fa55d422112c11701b0f7ce385dfa47
lapprenti18 <pokspid@gmail.com>
2021-12-08 11:29:19 +0100

    feat(MicsLevel): add mic icon and mute

--- src/Components/MicsLevel/MicsLevel.tsx
+++ src/Components/MicsLevel/MicsLevel.tsx
@@ -1,42 +1,54 @@
-import React from 'react';
-import ReactRanger from 'react-ranger'
-import CustomizedSlider from '../Slider/Slider'
+import React, { useState } from "react";
+import ReactRanger from "react-ranger";
+import CustomizedSlider from "../Slider/Slider";
 
 export const MicsLevel = () => {
-
-  const [exampleMicsArray] = React.useState([
+  const [exampleMicsArray, setExampleMicsArray] = React.useState([
     {
       id: 1,
-      name: 'Mic 1',
+      name: "Mic 1",
       level: 0,
       isActive: true,
     },
     {
       id: 2,
-      name: 'Mic 2',
+      name: "Mic 2",
       level: 50,
       isActive: true,
     },
     {
       id: 3,
-      name: 'Mic 3',
+      name: "Mic 3",
       level: 100,
       isActive: false,
-    }
+    },
   ]);
 
   const getData = (index: number, value: number) => {
     exampleMicsArray[index].level = value;
-    console.log('Values', exampleMicsArray);
-  }
+    console.log("Values", exampleMicsArray);
+  };
+
+  const setActive = (index: number, value: boolean) => {
+    let copy = exampleMicsArray.slice();
+    copy[index].isActive = value;
+    setExampleMicsArray(copy);
+
+  };
 
   return (
-      <>
-        {exampleMicsArray.map((item, index) => {
-          return (
-            <CustomizedSlider name={item.name} defaultValue={item.level} sendData={ (val: number) => getData(index, val)}/>
-          )
-        })}
-      </>
-  )
-}
+    <>
+      {exampleMicsArray.map((item, index) => {
+        return (
+          <CustomizedSlider
+            isActive={item.isActive}
+            name={item.name}
+            defaultValue={item.level}
+            sendData={(val: number) => getData(index, val)}
+            sendActive={(val: boolean) => setActive(index, val)}
+          />
+        );
+      })}
+    </>
+  );
+};
