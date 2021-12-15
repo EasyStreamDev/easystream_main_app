e2377349b7e98137e73beefa7bb2f6346c3ea894
Juan <alexandre.juan@epitech.eu>
2021-12-15 11:46:19 +0100

    fix(Testing): modify file path

--- src/Components/Testing/App.test.tsx
+++ src/Components/Testing/App.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import App from '../React/App'
+import App from '../App/App'
 
 test('renders learn react link', () => {
 	const { getByText } = render(<App />)
