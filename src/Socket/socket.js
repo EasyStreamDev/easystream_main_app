8fb238711be43cba433758d3d0664cf1e5257f39
Juan <alexandre.juan@epitech.eu>
2023-01-28 09:33:44 +0100

    debug(): get all mics

--- src/Socket/socket.js
+++ src/Socket/socket.js
@@ -60,6 +60,7 @@ class TCPConnection {
             this.sendData(obj, (data, error) => {
                 if (data) {
                     console.log('getAllMics resolve', data);
+                    console.log("DEBUG", data.mics[0])
                     resolve(data);
                 } else {
                     console.log('getAllMics error', error);
