e2d233aa160fcece1b7ab9b9c1ad7a80457ca156
Juan <alexandre.juan@epitech.eu>
2023-06-04 19:11:03 +0200

    refactor(): add code comments

--- src/Components/ActionsReactions/AddAppLaunch/AddAppLaunch.tsx
+++ src/Components/ActionsReactions/AddAppLaunch/AddAppLaunch.tsx
@@ -1,13 +1,13 @@
+// Importing necessary modules and components
 import { Box } from "@mui/system";
 import React from "react";
-import {
-  IoIosAddCircleOutline,
-} from "react-icons/io";
+import { IoIosAddCircleOutline } from "react-icons/io";
 import CSS from "csstype";
-import { AppLaunchModal } from "./AppLaunchModal/AppLaunchModal"
+import { AppLaunchModal } from "./AppLaunchModal/AppLaunchModal";
 import { SelectChangeEvent } from "@mui/material/Select";
 import { toast } from "react-toastify";
 
+// CSS styles for the Box component
 const boxStyles: CSS.Properties = {
   border: "solid",
   borderColor: "#FD7014",
@@ -22,30 +22,51 @@ const boxStyles: CSS.Properties = {
   cursor: "pointer",
 };
 
+// CSS styles for the icon
 const IconStyles: CSS.Properties = {
   paddingLeft: "15px",
   fontSize: "30px",
   width: "50px",
 };
 
+// CSS styles for the text
 const TextStyles: CSS.Properties = {
   display: "flex",
   alignItems: "center",
   textAlign: "center",
 };
 
+// Interface for the event object
 interface event {
   id: number;
   keyword: string[];
   source: String[];
 }
 
+/**
+ * Component for adding a new App Launch event
+ * @param props 
+ * @returns 
+ */
 export const AddAppLaunch = (props: any) => {
-  const [open, setOpen] = React.useState(false);
-  const [sources, setsources] = React.useState("");
+  const [open, setOpen] = React.useState(false); // State for managing modal open/close
+  const [sources, setsources] = React.useState(""); // State for managing sources
+
+  /**
+   * Function to handle opening the modal
+   * @returns 
+   */
   const handleOpen = () => setOpen(true);
+
+  /**
+   * Function to handle closing the modal
+   * @returns 
+   */
   const handleClose = () => setOpen(false);
 
+  /**
+   * Function to save the new event
+   */
   const save = () => {
     if (props.newEvent.length !== 0 && props.newEvent.source && props.newEvent.source.name) {
       props.addNewEvent(props.newEvent);
@@ -57,16 +78,22 @@ export const AddAppLaunch = (props: any) => {
     }
   };
 
+  /**
+   * Function to close the modal without saving
+   */
   const closeWithoutSave = () => {
     props.setnewEvent({ id: props.newEvent, keywords: [], sources: [] });
     handleClose();
   };
 
+  /**
+   * Function to handle input change
+   * @param e 
+   * @returns 
+   */
   const handleChange = (e: any) => {
     if (e.key === "Enter" && e.currentTarget.value !== "") {
-      
       let cpy = {...props.newEvent};
-      
       cpy.keywords[0] = (e.currentTarget.value);
       e.currentTarget.value = "";
       props.setnewEvent(cpy);
@@ -74,15 +101,19 @@ export const AddAppLaunch = (props: any) => {
     }
   };
 
+  /**
+   * Function to add a source
+   * @param event 
+   */
   const addSource = (event: SelectChangeEvent) => {
     let cpy = {...props.newEvent};
-  
     cpy.source = props.sources.find((elem: any) => elem.name === event.target.value)
     props.setnewEvent(cpy);
   };
 
   return (
     <>
+      {/* Box component for adding new event */}
       <Box style={boxStyles} onClick={handleOpen}>
         <h3 style={TextStyles}>
           Click to add new App Launch Event
@@ -91,6 +122,8 @@ export const AddAppLaunch = (props: any) => {
           </i>
         </h3>
       </Box>
+
+      {/* Modal component for adding a new event */}
       <AppLaunchModal
         handleOpen={handleOpen}
         handleClose={handleClose}
@@ -98,7 +131,6 @@ export const AddAppLaunch = (props: any) => {
         closeWithoutSave={closeWithoutSave}
         handleChange={handleChange}
         addSource={addSource}
-
         sources={props.sources}
         newEvent={props.newEvent}
         setnewEvent={props.setnewEvent}
