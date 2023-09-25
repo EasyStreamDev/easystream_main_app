ee6da69a3d14fbd2574ea673a9e644cf2e11d014
Juan <alexandre.juan@epitech.eu>
2023-09-25 21:59:16 +0200

    feat(subtitles): add edit mic from subtitle card

--- src/Components/Subtitles/Subtitles.tsx
+++ src/Components/Subtitles/Subtitles.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Chip, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, List, ListItem, ListItemText, OutlinedInput, Switch, TextField } from "@mui/material";
+import { Box, Button, Chip, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, IconButton, List, ListItem, ListItemText, OutlinedInput, Popover, Switch, TextField, Typography } from "@mui/material";
 import Divider from '@mui/material/Divider';
 import React, { Fragment, useEffect } from "react";
 import InputLabel from '@mui/material/InputLabel';
@@ -10,6 +10,7 @@ import { BsTrash } from "react-icons/bs";
 import "./Subtitles.css";
 import { Theme, useTheme } from '@mui/material/styles';
 import MicNoneIcon from '@material-ui/icons/MicNone';
+import AddIcon from '@material-ui/icons/Add';
 const ipcRenderer = window.require('electron').ipcRenderer
 
 const ITEM_HEIGHT = 48;
@@ -35,7 +36,18 @@ export const Subtitles = () => {
   const [load, setload] = React.useState(true);
   const [point, setpoint] = React.useState(".");
 
+  // Popover
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
+  const [listMicsAvailable, setListMicsAvailable] = React.useState<string[]>([]);
 
+  const handleClickPopover = (event: any, l: TextFieldSimple) => {
+    setAnchorEl(event.currentTarget);
+    setListMicsAvailable(micList.filter((mic) => !l.linked_mics.includes(mic)))
+  };
+
+  const handleClosePopover = () => {
+    setAnchorEl(null);
+  };
 
   const [subtitlesSettings, setSubtitlesSettings] = React.useState<TextFieldSimple[]>([]);
   const [availableTextFields, setAvailableTextFields] = React.useState<TextFieldDetailed[]>([]);
@@ -166,6 +178,48 @@ export const Subtitles = () => {
     });
   }
   
+  const refresh = (textField?: TextFieldDetailed) => {
+    getSubtitlesSettings().then((subtitles_res) => {
+      if (subtitles_res.statusCode === 200) {
+        console.log("getSubtitlesSettings", subtitles_res);
+        setSubtitlesSettings(subtitles_res.data.text_fields);
+        
+        if (textField !== undefined) {
+          // Remove the text field from availableTextFields
+          const availableTextFieldsCopy: TextFieldDetailed[] = availableTextFields.slice();
+          const index = availableTextFieldsCopy.findIndex((tf: TextFieldDetailed) => {
+            return tf.uuid === textField.uuid;
+          });
+
+          if (index !== -1) {
+            availableTextFieldsCopy.splice(index, 1);
+
+            setAvailableTextFields(availableTextFieldsCopy);
+
+            if (availableTextFields.length > 0) {
+              setNewSubtitleParam( JSON.stringify(availableTextFields[0]) )
+            }
+          }
+        }
+
+        // toast("SubtitlsetAvailableTextFieldse text field added !", {
+        //   type: "success",
+        // });
+
+        // Close the dialog
+        setOpen(false);
+
+        return;
+
+      } else {
+        toast("Error listing all subtitles settings. Verify the internet connection", {
+          type: "error",
+        });
+        return;
+      }
+    });
+  }
+
   const handleSave = () => {
     if (newSubtitleParam === "") {
       toast("You must select a text field to add", {
@@ -183,43 +237,7 @@ export const Subtitles = () => {
         setNewMicsListParam([]);
 
         // Refresh
-        getSubtitlesSettings().then((subtitles_res) => {
-          if (subtitles_res.statusCode === 200) {
-            console.log("getSubtitlesSettings", subtitles_res);
-            setSubtitlesSettings(subtitles_res.data.text_fields);
-            
-            // Remove the text field from availableTextFields
-            const availableTextFieldsCopy: TextFieldDetailed[] = availableTextFields.slice();
-            const index = availableTextFieldsCopy.findIndex((tf: TextFieldDetailed) => {
-              return tf.uuid === textField.uuid;
-            });
-
-            if (index !== -1) {
-              availableTextFieldsCopy.splice(index, 1);
-
-              setAvailableTextFields(availableTextFieldsCopy);
-
-              if (availableTextFields.length > 0) {
-                setNewSubtitleParam( JSON.stringify(availableTextFields[0]) )
-              }
-            }
-
-            // toast("Subtitle text field added !", {
-            //   type: "success",
-            // });
-
-            // Close the dialog
-            setOpen(false);
-
-            return;
-
-          } else {
-            toast("Error listing all subtitles settings. Verify the internet connection", {
-              type: "error",
-            });
-            return;
-          }
-        });
+        refresh(textField);
       } else {
         toast("Error adding subtitle text field", {
           type: "error",
@@ -227,6 +245,27 @@ export const Subtitles = () => {
         return;
       }
     });
+  }
+
+  const addMicToSubtitle = (text_field: TextFieldSimple, mic_to_add: any) => {
+    let new_mics = text_field.linked_mics.slice();
+    new_mics.push(mic_to_add);
+
+    addSubtitleTextField(text_field.uuid, new_mics).then((res: resultFormat) => {
+      if (res.statusCode === 200) {
+        // Refresh
+        refresh();
+        toast("Mic added to the text field", {
+          type: "success",
+        });
+      } else {
+        toast("Error while adding the mic to the text field", {
+          type: "error",
+        });
+      }
+    });
+    refresh();
+    handleClosePopover();
   }
 
   const deleteSubtitleTextField = (uuid: string) => {
@@ -419,7 +458,7 @@ export const Subtitles = () => {
                             <ListItem
                               key={l.uuid}
                               secondaryAction={
-                                <IconButton onClick={deleteSubtitleTextField(l.uuid)} edge="end" color="warning" aria-label="delete">
+                                <IconButton onClick={deleteSubtitleTextField(l.uuid)} edge="end" color="error" aria-label="delete">
                                   <BsTrash />
                                 </IconButton>
                               }
@@ -431,13 +470,59 @@ export const Subtitles = () => {
                             <Box display="flex" justifyContent="center" m={1} p={1}>
                               {
                                 l.linked_mics.map((value) => (
-                                  <Chip key={value} label={value} sx={{ m: 0.5, backgroundColor: "#FFA500" }}
-                                  icon={<MicNoneIcon />} onDelete={handleMicDelete(l.uuid, value)}
+                                  <Chip className="color-white" key={value} label={value} variant="outlined" sx={{ m: 0.5, borderColor: "#FFA500" }}
+                                  icon={<MicNoneIcon className="color-orange" />} onDelete={handleMicDelete(l.uuid, value)}
                                   />
                                 ))
                               }
+                              <Chip key="+" label="+" className="color-orange" sx={{ m: 0.5, fontSize: "20px", borderColor: "#FFA500" }}
+                                onClick={event => handleClickPopover(event, l)}
+                                />
+                                {
+                                  Boolean(anchorEl) &&
+                                  <Popover
+                                    id={l.uuid}
+                                    open={Boolean(anchorEl)}
+                                    anchorEl={anchorEl}
+                                    onClose={handleClosePopover}
+                                    anchorOrigin={{
+                                      vertical: 'center',
+                                      horizontal: 'right',
+                                    }}
+                                    transformOrigin={{
+                                      vertical: 'center',
+                                      horizontal: 'left',
+                                    }}
+                                  >
+                                    {
+                                      listMicsAvailable.length === 0 ? (
+                                        <Typography sx={{ p: 2 }}>No mic available</Typography>
+                                      ) :
+                                        (
+                                          <div>
+                                            <Typography sx={{ p: 2 }}>Select a mic:</Typography>
+                                            <List>
+                                              { listMicsAvailable.map((choice) => (
+                                                <ListItem
+                                                  key={choice}
+                                                  onClick={() => addMicToSubtitle(l, choice)}
+                                                >
+                                                  <ListItemText primary={choice} />
+                                                </ListItem>
+                                              ))}
+                                            </List>
+                                          </div>
+                                        )
+                                    }
+                                  </Popover>
+                                }
                             </Box>
-                            <Divider style={{ border: "1.5px solid orange" }} variant="middle" />
+                            {/* Show only if it's not the last element */}
+                            {
+                              subtitlesSettings[subtitlesSettings.length - 1] !== l && (
+                                <Divider style={{ border: "1.5px solid orange", backgroundColor: "orange" }} />
+                              )
+                            }
                           </Box>
                         )
                       })
