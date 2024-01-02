b060a347fd27084daeef83c79dd75d3d61b385bd
Juan <alexandre.juan@epitech.eu>
2024-01-02 21:08:12 +0100

    fix update when removing

--- src/Components/LinkMicToVideoSource/LinkMicToVideoSource.tsx
+++ src/Components/LinkMicToVideoSource/LinkMicToVideoSource.tsx
@@ -35,6 +35,7 @@ import {
   resultFormat,
 } from "../../Socket/interfaces";
 import "./LinkMicToVideoSource.css";
+import { get } from "http";
 const ipcRenderer = window.require("electron").ipcRenderer;
 
 const ITEM_HEIGHT = 48;
@@ -221,8 +222,11 @@ export const LinkMicToVideoSource = (props: any) => {
   const handleMicDelete = (micUuid: string) => () => {
     removeMicToVideoSource(micUuid)
       .then((res) => {
-        toast("Mic unlinked to display source.", {
-          type: "success",
+        getAllLinksMicsToVideoSource().then((res) => {
+          setAllLinksMicsToVideoSource(res);
+          toast("Mic unlinked to display source.", {
+            type: "success",
+          });
         });
       })
       .catch((err) => {
